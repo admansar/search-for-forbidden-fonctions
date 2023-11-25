79b5f180f44150c6e81701989427caa9a904e0d7
Adnane Mansar <admansar@c1r7p6.1337.ma>
2023-11-26 00:52:44 +0100

    test

--- t.sh
+++ t.sh
@@ -3,8 +3,10 @@
 if [ $# -eq 0 ] || [ $# -gt 1 ] 
 then
 	echo -e "\033[0;33m"
-	echo "enter your code name (.c file) after the script name, just one " 
+	echo "usage : enter your code name (a.out file) after this script"
+	echo "something like --> bash t.sh ./a.out"
 	echo -e "\033[0m"
 else
-./check $(cat $1)
+chmod +x check
+./check $1
 fi
